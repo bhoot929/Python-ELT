6d3f8fec3d1ac60395f4b8ceb2e25f75b45f79d0
Swapnil <30797433+bhoot929@users.noreply.github.com>
2018-08-30 14:34:49 +0530

    Create Fill_string_in_sentence.py

---
+++ Fill_string_in_sentence.py
@@ -0,0 +1,16 @@
+# this code will fill string in emtpy space in a setence:
+
+
+def join(word,replace):
+    l = list()
+    for i in range(len(word)):
+        if(word[i] == ' '):
+            l.append(replace)
+        else:
+            l.append(word[i])
+    return ''.join(l)
+print(join('Join the Stack Overflow Community' , '%20'))
+
+'''
+output:Join%20the%20Stack%20Overflow%20Community
+'''
